4b237373654af2828fbadbcf611d504a5aa664b8
tsantek <santek.vg@gmail.com>
2019-06-17 16:30:16 -0700

    tests ejs

--- controllers/template.js
+++ controllers/template.js
@@ -21,13 +21,18 @@ module.exports = {
                 knex('tests')
                     .where('recruiters_id', req.params.id)
                     .then((test) => {
-                        knex(`tests_complited`)
-                            .join('users', 'tests_complited.test_id', 'users.id')
-                            .join('tests', 'tests_complited.test_id', 'tests.id')
-                            .select('tests.name', 'tests_complited.total', 'tests_complited.correct', 'users.name AS user_Name', 'users.email')
-                            .where('tests_complited.recruiters_id', req.params.id)
-                            .then((result) => {
-                                res.render("pages/dashboard", { user, test, result });
+                        knex('recruiters')
+                            .where('id', req.params.id)
+                            .then((recruiter) => {
+                                knex(`tests_complited`)
+                                    .join('users', 'tests_complited.test_id', 'users.id')
+                                    .join('tests', 'tests_complited.test_id', 'tests.id')
+                                    .select('tests.name', 'tests_complited.total', 'tests_complited.correct', 'users.name AS user_Name', 'users.email')
+                                    .where('tests_complited.recruiters_id', req.params.id)
+                                    .then((result) => {
+                                        console.log(recruiter)
+                                        res.render("pages/dashboard", { user, test, result, recruiter });
+                                    })
                             })
                     })
             })
@@ -81,13 +86,13 @@ module.exports = {
         //     res.render("/pages/login")
         //   })
         knex('users')
-        .where('recruiters_id', req.params.id)
-        .then((user) => {
-            knex('tests')
-                .then((test) => {
-                    console.log(test)
-                    res.render('pages/userPage', {user, test})
-                })
-        })
+            .where('recruiters_id', req.params.id)
+            .then((user) => {
+                knex('tests')
+                    .then((test) => {
+                        console.log(test)
+                        res.render('pages/userPage', { user, test })
+                    })
+            })
     },
 }
