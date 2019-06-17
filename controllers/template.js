316a3a477273f2d99e9421e74b97f716edf7ca4c
Ryan Jones <ryanjones33@gmail.com>
2019-06-17 16:46:57 -0700

    user link route

--- controllers/template.js
+++ controllers/template.js
@@ -72,14 +72,6 @@ module.exports = {
         });
     },
     userDashboard: (req, res) => {
-        // knex('users')
-        //   .join('tests_complited', 'users.id', '=', 'tests_complited.user_id')
-        //   .where('tests_complited.recruiters_id', req.params.rid)
-        //   .where('users.id', req.params.uid)
-        //   .then(test => {
-        //     console.log('this is test', test)
-        //     res.render("/pages/login")
-        //   })
         knex('users')
             .where('recruiters_id', req.params.rid)
             .then((user) => {
