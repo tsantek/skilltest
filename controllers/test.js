37f328177b63448b8734622aa0c5eea7d3aef25d
bradtxufl <brad.w@utexas.edu>
2019-06-18 10:09:32 -0700

    creating pull

--- controllers/test.js
+++ controllers/test.js
@@ -2,16 +2,28 @@ const knex = require("../db/knex.js");
 
 module.exports = {
   create: (req, res) => {
-      knex('users')
-          .where('recruiters_id', req.params.id)
-          .then((user) => {
-              knex('tests')
-                  .where('recruiters_id', req.params.id)
-                  .then((test) => {
-                      res.render("pages/dashboardcreate", { user, test });
-                  })
-          })
-  },
+          knex('users')
+              .where('recruiters_id', req.params.id)
+              .then((user) => {
+                  knex('tests')
+                      .where('recruiters_id', req.params.id)
+                      .then((test) => {
+                          knex('recruiters')
+                              .where('id', req.params.id)
+                              .then((recruiter) => {
+                                  knex(`tests_complited`)
+                                      .join('users', 'tests_complited.test_id', 'users.id')
+                                      .join('tests', 'tests_complited.test_id', 'tests.id')
+                                      .select('tests.name', 'tests_complited.total', 'tests_complited.correct', 'users.name AS user_Name', 'users.email')
+                                      .where('tests_complited.recruiters_id', req.params.id)
+                                      .then((result) => {
+                                          console.log(recruiter)
+                                          res.render("pages/dashboardcreate", { user, test, result, recruiter });
+                                      })
+                              })
+                      })
+              })
+      },
 
   question: (req, res) => {
       var q = {
