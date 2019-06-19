64eef8fbf472c11f9f55787a21c007abeec1239b
tsantek <santek.vg@gmail.com>
2019-06-19 13:55:03 -0700

    clean conosle.log

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,6 @@ module.exports = {
                                     .select('tests.name', 'tests_completed.total', 'tests_completed.correct', 'users.name AS user_Name', 'users.email')
                                     .where('tests_completed.recruiters_id', req.params.id)
                                     .then((result) => {
-                                      console.log({ user, test, result, recruiter })
                                         res.render("pages/dashboardAddUser", { user, test, result, recruiter });
                                     })
                             })
@@ -26,44 +25,43 @@ module.exports = {
     },
 
     createUser: (req, res) => {
-      knex('users').insert({
-          name: req.body.name,
-          email: req.body.email,
-          recruiters_id: req.params.id,
-          notes: []
+        knex('users').insert({
+            name: req.body.name,
+            email: req.body.email,
+            recruiters_id: req.params.id,
+            notes: []
         }).returning('*').then((result) => {
-          var tid = parseInt(result[0].id);
+            var tid = parseInt(result[0].id);
             knex('users')
-              .where('recruiters_id', req.params.id)
-              .then((user) => {
-                  knex('tests')
-                      .where('recruiters_id', req.params.id)
-                      .then((test) => {
-                          knex('recruiters')
-                              .where('id', req.params.id)
-                              .then((recruiter) => {
-                                  knex(`tests_completed`)
-                                      .join('users', 'tests_completed.test_id', 'users.id')
-                                      .join('tests', 'tests_completed.test_id', 'tests.id')
-                                      .select('tests.name', 'tests_completed.total', 'tests_completed.correct', 'users.name AS user_Name', 'users.email')
-                                      .where('tests_completed.recruiters_id', req.params.id)
-                                      .then((result) => {
-                                        console.log({ user, test, result, recruiter })
-                                          res.render("pages/dashboard", { user, test, result, recruiter });
-                                      })
-                              })
-                      })
-              })
-            })
+                .where('recruiters_id', req.params.id)
+                .then((user) => {
+                    knex('tests')
+                        .where('recruiters_id', req.params.id)
+                        .then((test) => {
+                            knex('recruiters')
+                                .where('id', req.params.id)
+                                .then((recruiter) => {
+                                    knex(`tests_completed`)
+                                        .join('users', 'tests_completed.test_id', 'users.id')
+                                        .join('tests', 'tests_completed.test_id', 'tests.id')
+                                        .select('tests.name', 'tests_completed.total', 'tests_completed.correct', 'users.name AS user_Name', 'users.email')
+                                        .where('tests_completed.recruiters_id', req.params.id)
+                                        .then((result) => {
+                                            res.render("pages/dashboard", { user, test, result, recruiter });
+                                        })
+                                })
+                        })
+                })
+        })
     },
 
-  addNote: (req, res) => {
-    knex('users')
-      .where('id', req.params.uid)
-      .update({
-        notes: knex.raw('array_append(notes, ?)', [req.body.note])
-      }).then(() => {
-        res.redirect(`/dashboard/${req.params.rid}/user/${req.params.uid}`)
-      })
+    addNote: (req, res) => {
+        knex('users')
+            .where('id', req.params.uid)
+            .update({
+                notes: knex.raw('array_append(notes, ?)', [req.body.note])
+            }).then(() => {
+                res.redirect(`/dashboard/${req.params.rid}/user/${req.params.uid}`)
+            })
     }
-  }
+}
