587503dcac52c5abb63a50e91d352eb0d574242b
bradtxufl <brad.w@utexas.edu>
2019-06-20 15:21:18 -0700

    test order

--- db/seeds/02_tests.js
+++ db/seeds/02_tests.js
@@ -5,8 +5,9 @@ exports.seed = function(knex, Promise) {
             // Inserts seed entries
             return knex('tests').insert([
                 { name: "JavaScript", total: 10, code: "sdsa2342", recruiters_id: 1 },
-                { name: "Java", total: 10, code: "sds11122342", recruiters_id: 1 },
-                { name: "Node.js", total: 10, code: "123sdasada", recruiters_id: 1 }
+                { name: "Node.js", total: 10, code: "123sdasada", recruiters_id: 1 },
+                { name: "Java", total: 10, code: "sds11122342", recruiters_id: 1 }
+
             ]);
         });
 };
