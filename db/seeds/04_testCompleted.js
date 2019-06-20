8680463e7c190f3dae520ed3d201f32eeaf6ff9d
tsantek <santek.vg@gmail.com>
2019-06-20 10:53:22 -0700

    update seeds

--- db/seeds/04_testCompleted.js
+++ db/seeds/04_testCompleted.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
         .then(function() {
             // Inserts seed entries
             return knex('tests_completed').insert([
-                { test_id: 1, user_id: 1, total: 5, correct: 4, completed: false, recruiters_id: 1 },
+                { test_id: 1, user_id: 1, total: 5, correct: 0, completed: false, recruiters_id: 1 },
                 { test_id: 2, user_id: 1, total: 10, correct: 4, completed: true, recruiters_id: 1 }
             ]);
         });
