09069c1ba2ffc1d060a5c50f360ae433f6c96bd2
Ryan Jones <ryanjones33@gmail.com>
2019-06-21 10:26:22 -0700

    Updated user seed

--- db/seeds/04_testCompleted.js
+++ db/seeds/04_testCompleted.js
@@ -4,6 +4,8 @@ exports.seed = function(knex, Promise) {
         .then(function() {
             // Inserts seed entries
             return knex('tests_completed').insert([
+                { test_id: 4, user_id: 1, total: 10, correct: 8, completed: true, recruiters_id: 1 },
+                { test_id: 5, user_id: 1, total: 10, correct: 9, completed: true, recruiters_id: 1 },
                 { test_id: 1, user_id: 1, total: 10, correct: 7, completed: true, recruiters_id: 1 },
                 { test_id: 2, user_id: 1, total: 10, correct: 8, completed: true, recruiters_id: 1 },
                 { test_id: 3, user_id: 1, total: 10, correct: 10, completed: true, recruiters_id: 1 },
